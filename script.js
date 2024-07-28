d319186587396a085820777a7bbdb3b64c850cc8
salomeelbakidze <elbakidzesalome23@gmail.com>
2024-07-29 00:32:09 +0400

    js

---
+++ script.js
@@ -0,0 +1,33 @@
+// ...1
+
+let newNumber = 100;
+
+if (newNumber < 50) {
+    console.log("less than 50");
+} else if (newNumber > 20) {
+    console.log(" more than 20");
+} else {
+    console.log("error");
+}
+
+
+// ...2
+
+let userName = "username == mariam" ? "true" : "false" ;
+
+
+// ...3
+
+
+let username = "mariam";
+
+switch (username) {
+    case mariam:
+        console.log("true"); 
+        
+        break;
+
+    default: 
+    console.log("false");
+        
+}
